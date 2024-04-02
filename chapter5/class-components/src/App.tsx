3567c0ad9b3b6336d60212544cc972d22629539f
ekuzmichev <evgenii.e.kuzmichev@gmail.com>
2024-04-02 08:47:26 +0300

    Add Greeting component

--- chapter5/class-components/src/App.tsx
+++ chapter5/class-components/src/App.tsx
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Greeting from './Greeting';
+import logo from './logo.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props: any) {
+    super(props)
+
+    this.state = {
+      enteredName: ""
+    }
+
+    this.onNameChange = this.onNameChange.bind(this)
+  }
+
+  state: { enteredName: string }
+
+  onNameChange(e: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({
+      enteredName: e.target.value
+    })
+  }
+
+  render() {
+    console.log("Rendering App")
+    return (
+      <div className="App" >
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <input value={this.state.enteredName} onChange={this.onNameChange} />
+          <Greeting name={this.state.enteredName} />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
