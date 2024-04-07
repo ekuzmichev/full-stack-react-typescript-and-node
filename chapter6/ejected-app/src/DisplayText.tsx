c6cca8d2597d470470f6fce788d968a63032a7e1
ekuzmichev <evgenii.e.kuzmichev@gmail.com>
2024-04-07 12:38:28 +0300

    Add todos fetching

--- chapter6/ejected-app/src/DisplayText.tsx
+++ chapter6/ejected-app/src/DisplayText.tsx
@@ -7,6 +7,7 @@ interface DisplayTextProps {
 const DisplayText: FC<DisplayTextProps> = ({ getUserFullName }) => {
     const [txt, setTxt] = useState("")
     const [msg, setMsg] = useState("")
+    const [todos, setTodos] = useState<React.JSX.Element[]>()
 
     const onTextInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setTxt(e.target.value)
@@ -15,6 +16,26 @@ const DisplayText: FC<DisplayTextProps> = ({ getUserFullName }) => {
     const onShowMsgBtnClick = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         e.preventDefault()
         setMsg(`Welcome to React testing, ${await getUserFullName(txt)}`)
+        setUserTodos()
+    }
+
+    const setUserTodos = async () => {
+        const userResponse: Response = await fetch('https://jsonplaceholder.typicode.com/users')
+        if (userResponse.ok) {
+            const users = await userResponse.json()
+            const userByName = users.find((user: any) => {
+                return user.username.toLowerCase() === txt.toLowerCase()
+            })
+            console.log("user by username", userByName)
+            const todosResponse: Response = await fetch('https://jsonplaceholder.typicode.com/todos')
+            if (todosResponse.ok) {
+                const todos = await todosResponse.json()
+                const userTodos = todos.filter((todo: any) => todo.userId === userByName.id)
+                console.log("user todos", userTodos)
+                const todoList = userTodos.map((todo: any) => <li key={todo.id}>{todo.title}</li>)
+                setTodos(todoList)
+            }
+        }
     }
 
     return (
@@ -31,6 +52,7 @@ const DisplayText: FC<DisplayTextProps> = ({ getUserFullName }) => {
             <div>
                 <label data-testid="final-msg">{msg}</label>
             </div>
+            <ul style={{ marginTop: '1rem', listStyleType: 'none' }}>{todos}</ul>
         </form>
     )
 }
