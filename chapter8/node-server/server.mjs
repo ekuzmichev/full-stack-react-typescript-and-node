8dc1517068d20af0f6648d91677cb6e83fb9bae9
ekuzmichev <evgenii.e.kuzmichev@gmail.com>
2024-04-13 10:23:54 +0300

    Add POST processing example

--- chapter8/node-server/server.mjs
+++ chapter8/node-server/server.mjs
@@ -7,6 +7,16 @@ const server = http.createServer((req, res) /*Request, Response*/ => {
     res.end("welcome to route a");
   } else if (req.url === "/b") {
     res.end("welcome to route b");
+  } else if (req.url === "/c" && req.method === "POST") {
+    let body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const params = Buffer.concat(body);
+      console.log("body", params.toString());
+      res.end(`You submitted this parameters: ${params.toString()}`);
+    });
   } else {
     res.end("good bye");
   }
