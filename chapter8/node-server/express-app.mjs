cc2987aa685cb2d2aeafc045b0bd7a7db0f92084
ekuzmichev <evgenii.e.kuzmichev@gmail.com>
2024-04-14 10:00:03 +0300

    Add error processing app middleware

--- chapter8/node-server/express-app.mjs
+++ chapter8/node-server/express-app.mjs
@@ -1,4 +1,5 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 
@@ -9,15 +10,22 @@ router.get("/a", (req, res, next) => {
 });
 
 router.post("/c", (req, res, next) => {
-  res.send("Hello! This is route c");
+  res.send("Hello! This is route c. The message is: " + req.body.message);
 });
+
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   console.log("First middleware");
   next();
+  // throw new Error("A failure occured!"); // To check last middleware that is intended to process errors
 });
 
 app.use(router);
+
+app.use((err, req, res, next) => {
+  res.status(500).send(err.message);
+});
 
 app.listen({ port: 8000 }, () => {
   console.log("Express Node server has loaded!");
