9a69b4e3a6eeb4e812cfdfee487637506cb228cc
ekuzmichev <evgenii.e.kuzmichev@gmail.com>
2024-04-14 09:45:57 +0300

    Add middleware examples

--- chapter8/node-server/express-app.mjs
+++ chapter8/node-server/express-app.mjs
@@ -2,6 +2,15 @@ import express from "express";
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("First middleware");
+  next();
+});
+
+app.use((req, res, next) => {
+  res.send("Hello world. I am a custom middleware."); // Sends also "text/html" header to client
+});
+
 app.listen({ port: 8000 }, () => {
   console.log("Express Node server has loaded!");
 });
