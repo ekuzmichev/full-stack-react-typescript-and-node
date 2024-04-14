01f4b5d7bd16e3c1ee8664d2245f82c1a042adc8
ekuzmichev <evgenii.e.kuzmichev@gmail.com>
2024-04-14 09:52:05 +0300

    Add router with path processing middlewares

--- chapter8/node-server/express-app.mjs
+++ chapter8/node-server/express-app.mjs
@@ -2,14 +2,22 @@ import express from "express";
 
 const app = express();
 
+const router = express.Router();
+
+router.get("/a", (req, res, next) => {
+  res.send("Hello! This is route a");
+});
+
+router.post("/c", (req, res, next) => {
+  res.send("Hello! This is route c");
+});
+
 app.use((req, res, next) => {
   console.log("First middleware");
   next();
 });
 
-app.use((req, res, next) => {
-  res.send("Hello world. I am a custom middleware."); // Sends also "text/html" header to client
-});
+app.use(router);
 
 app.listen({ port: 8000 }, () => {
   console.log("Express Node server has loaded!");
