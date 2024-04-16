582d66eafa7e95278194fb17d7d2469683cc9703
ekuzmichev <evgenii.e.kuzmichev@gmail.com>
2024-04-16 13:15:22 +0300

    Add addTodo mutation

--- chapter9/graphql-schema/resolvers.ts
+++ chapter9/graphql-schema/resolvers.ts
@@ -1,14 +1,15 @@
 import { IResolvers } from "apollo-server-express";
 import { v4 } from "uuid";
 import { GqlContext } from "./GqlContext";
+import { todos } from "./db";
 
-interface User {
+export interface User {
   id: string;
   username: string;
   email?: string;
 }
 
-interface Todo {
+export interface Todo {
   id: string;
   title: string;
   description?: string;
@@ -35,22 +36,25 @@ const resolvers: IResolvers = {
       ctx: GqlContext,
       info: any
     ): Promise<Todo[]> => {
-      return [
-        {
-          id: v4(),
-          title: "First todo",
-          description: "First todo description",
-        },
-        {
-          id: v4(),
-          title: "Second todo",
-          description: "Second todo description",
-        },
-        {
-          id: v4(),
-          title: "Third todo",
-        },
-      ];
+      return todos;
+    },
+  },
+  Mutation: {
+    addTodo: async (
+      parent: any,
+      args: {
+        title: string;
+        description: string;
+      },
+      ctx: GqlContext,
+      info: any
+    ): Promise<Todo> => {
+      todos.push({
+        id: v4(),
+        title: args.title,
+        description: args.description,
+      });
+      return todos[todos.length - 1];
     },
   },
 };
