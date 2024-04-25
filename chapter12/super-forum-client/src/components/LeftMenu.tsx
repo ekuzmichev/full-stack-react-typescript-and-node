898f2c88cbe796bb22d9af4989f4286fb74a2b06
ekuzmichev <evgenii.e.kuzmichev@gmail.com>
2024-04-25 09:16:48 +0300

    Add pre-loading of categories into LeftMenu panel

--- chapter12/super-forum-client/src/components/LeftMenu.tsx
+++ chapter12/super-forum-client/src/components/LeftMenu.tsx
@@ -1,14 +1,32 @@
+import { useEffect, useState } from "react";
 import { MIN_DESKTOP_WINDOW_WIDTH } from "../constants";
 import { useWindowDimensions } from "../hooks/useWindowDimensions";
+import { getCategories } from "../services/DataService";
+import Category from "../models/Category";
 
 const LeftMenu = () => {
   const { width } = useWindowDimensions();
+
+  const [categories, setCategories] = useState<JSX.Element>(
+    <div>Left Menu</div>
+  );
+
+  useEffect(() => {
+    getCategories()
+      .then((categories: Category[]) => {
+        const categoryListItems: JSX.Element[] = categories.map((category) => {
+          return <li key={category.id}>{category.name}</li>;
+        });
+        setCategories(<ul className="category">{categoryListItems}</ul>);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   if (width <= MIN_DESKTOP_WINDOW_WIDTH) {
     return null;
   }
 
-  return <div className="left-menu">Left Menu</div>;
+  return <div className="left-menu">{categories}</div>;
 };
 
 export default LeftMenu;
