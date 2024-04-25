b58b73e0609ea1e3a0b26e0b5efef8b3b0a842b5
ekuzmichev <evgenii.e.kuzmichev@gmail.com>
2024-04-25 09:09:44 +0300

    Add fake backend tmp replacement & models

---
+++ chapter12/super-forum-client/src/services/DataService.ts
@@ -0,0 +1,202 @@
+import Category from "../models/Category";
+import CategoryThread from "../models/CategoryThread";
+import Thread from "../models/Thread";
+
+export async function getCategories(): Promise<Category[]> {
+  const promise = new Promise<Category[]>((res, rej) => {
+    setTimeout(() => {
+      const categories: Category[] = [
+        new Category("1", "Programming"),
+        new Category("2", "Cooking"),
+        new Category("3", "Sports"),
+        new Category("4", "Entertainment"),
+        new Category("5", "Travel"),
+      ];
+
+      res(categories);
+    }, 2000);
+  });
+  return promise;
+}
+
+export async function getThreadsByCategory(catId: string): Promise<Thread[]> {
+  const promise = new Promise<Thread[]>((res, rej) => {
+    setTimeout(() => {
+      const threads: Thread[] = [];
+      threads.push({
+        id: "1",
+        views: 22,
+        title: "Thread 1",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        userName: "dave",
+        userId: "1",
+        points: 11,
+        createdOn: new Date(),
+        lastModifiedOn: new Date(),
+        threadItems: [
+          {
+            id: "1",
+            views: 22,
+            points: 2,
+            body: "ThreadItem 1",
+            userName: "jon",
+            userId: "2",
+            createdOn: new Date(),
+            threadId: "1",
+          },
+        ],
+        category: new Category("1", "Programming"),
+      });
+      threads.push({
+        id: "2",
+        views: 2,
+        title: "Thread 2",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        userName: "tim",
+        userId: "2",
+        points: 55,
+        createdOn: new Date(),
+        lastModifiedOn: new Date(),
+        threadItems: [
+          {
+            id: "1",
+            views: 22,
+            points: 2,
+            body: "ThreadItem 1",
+            userName: "jon",
+            userId: "2",
+            createdOn: new Date(),
+            threadId: "2",
+          },
+        ],
+        category: new Category("1", "Programming"),
+      });
+
+      res(threads);
+    }, 2000);
+  });
+  return promise;
+}
+
+export async function getTopCategories(): Promise<CategoryThread[]> {
+  const promise = new Promise<CategoryThread[]>((res, rej) => {
+    setTimeout(() => {
+      const topCategories: CategoryThread[] = [
+        new CategoryThread("1", "Programming", "How can I learn JavaScript"),
+        new CategoryThread("2", "Programming", "How can I learn Node"),
+        new CategoryThread("3", "Programming", "How can I learn React"),
+        new CategoryThread("4", "Cooking", "How do I learn French cuisine?"),
+        new CategoryThread("5", "Cooking", "How do I learn Italian cuisine?"),
+        new CategoryThread("6", "Sports", "How can I learn to play Soccer"),
+        new CategoryThread("7", "Sports", "How can I learn to play Basketball"),
+        new CategoryThread("8", "Sports", "How can I learn to play Baseball"),
+      ];
+
+      res(topCategories);
+    }, 2000);
+  });
+  return promise;
+}
+
+export async function getThreadById(Id: string): Promise<Thread> {
+  const promise = new Promise<Thread>((res, rej) => {
+    setTimeout(() => {
+      const thread = {
+        id: "1",
+        views: 22,
+        title: "Thread 1",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        userName: "dave",
+        userId: "1",
+        points: 11,
+        createdOn: new Date(),
+        lastModifiedOn: new Date(),
+        threadItems: [
+          {
+            id: "1",
+            views: 22,
+            points: 2,
+            body: "ThreadItem 1",
+            userName: "jon",
+            userId: "2",
+            createdOn: new Date(),
+            threadId: "1",
+          },
+          {
+            id: "2",
+            views: 11,
+            points: 14,
+            body: "ThreadItem 2",
+            userName: "linda",
+            userId: "4",
+            createdOn: new Date(),
+            threadId: "1",
+          },
+        ],
+        category: new Category("1", "Programming"),
+      };
+
+      res(thread);
+    }, 2000);
+  });
+  return promise;
+}
+
+export async function getUserThreads(id: string): Promise<Thread[]> {
+  const result = new Promise<Thread[]>((res) => {
+    setTimeout(() => {
+      const threads: Thread[] = [];
+      threads.push({
+        id: "1",
+        views: 22,
+        title: "Thread 1",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        userName: "dave",
+        userId: "1",
+        points: 11,
+        createdOn: new Date(),
+        lastModifiedOn: new Date(),
+        threadItems: [
+          {
+            id: "1",
+            views: 22,
+            points: 2,
+            body: "ThreadItem 1",
+            userName: "jon",
+            userId: "2",
+            createdOn: new Date(),
+            threadId: "1",
+          },
+        ],
+        category: new Category("1", "Programming"),
+      });
+      threads.push({
+        id: "2",
+        views: 2,
+        title: "Thread 2",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        userName: "tim",
+        userId: "2",
+        points: 55,
+        createdOn: new Date(),
+        lastModifiedOn: new Date(),
+        threadItems: [
+          {
+            id: "1",
+            views: 22,
+            points: 2,
+            body: "ThreadItem 1",
+            userName: "jon",
+            userId: "2",
+            createdOn: new Date(),
+            threadId: "2",
+          },
+        ],
+        category: new Category("1", "Programming"),
+      });
+
+      res(threads);
+    }, 2000);
+  });
+  return result;
+}
