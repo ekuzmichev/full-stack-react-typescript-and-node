c1040e521a87869388cea980eb91fd0ee3a457da
ekuzmichev <evgenii.e.kuzmichev@gmail.com>
2024-04-26 09:54:49 +0300

    Add menu modal

--- chapter12/super-forum-client/src/components/nav/Nav.tsx
+++ chapter12/super-forum-client/src/components/nav/Nav.tsx
@@ -1,26 +1,58 @@
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 import { MIN_DESKTOP_WINDOW_WIDTH } from "../../constants";
 import { useWindowDimensions } from "../../hooks/useWindowDimensions";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
 import "./Nav.css";
+import ReactModal from "react-modal";
+import { SidebarMenus } from "../sidebar/SidebarMenus";
 
 const Nav = () => {
+  const [menuVisible, setMenuVisible] = useState(false);
+
   const { width } = useWindowDimensions();
+
+  const onMenuIconClick = (e: React.MouseEvent<Element, MouseEvent>) => {
+    setMenuVisible(!menuVisible);
+  };
+
+  const onMenuModalRequestClose = (
+    e: React.MouseEvent<Element, MouseEvent> | React.KeyboardEvent<Element>
+  ) => {
+    setMenuVisible(false);
+  };
 
   const getMobileMenu = () => {
     if (width <= MIN_DESKTOP_WINDOW_WIDTH) {
       return (
-        <FontAwesomeIcon icon={faBars} size="lg" className="nav-mobile-menu" />
+        <FontAwesomeIcon
+          icon={faBars}
+          onClick={onMenuIconClick}
+          size="lg"
+          className="nav-mobile-menu"
+        />
       );
     }
     return null;
   };
 
   return (
-    <div className="nav">
-      {getMobileMenu()}
-      <strong>SuperForum</strong>
-    </div>
+    <>
+      <ReactModal
+        className="modal-menu"
+        isOpen={menuVisible}
+        onRequestClose={onMenuModalRequestClose}
+        shouldCloseOnOverlayClick={true}
+        shouldCloseOnEsc={true}
+        ariaHideApp={false}
+      >
+        <SidebarMenus />
+      </ReactModal>
+      <div className="nav">
+        {getMobileMenu()}
+        <strong>SuperForum</strong>
+      </div>
+    </>
   );
 };
 
