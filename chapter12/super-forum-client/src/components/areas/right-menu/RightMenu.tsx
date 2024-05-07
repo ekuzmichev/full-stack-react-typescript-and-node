8767e6d42ad6df4e3952c2f4f7877fbe66c62a9c
ekuzmichev <evgenii.e.kuzmichev@gmail.com>
2024-05-07 10:36:42 +0300

    Fill RightMenu

--- chapter12/super-forum-client/src/components/areas/right-menu/RightMenu.tsx
+++ chapter12/super-forum-client/src/components/areas/right-menu/RightMenu.tsx
@@ -1,12 +1,40 @@
+import { Dictionary } from "lodash";
+import groupBy from "lodash/groupBy";
+import { useEffect, useState } from "react";
 import { MIN_DESKTOP_WINDOW_WIDTH } from "../../../constants";
 import { useWindowDimensions } from "../../../hooks/useWindowDimensions";
+import { CategoryThread } from "../../../models/CategoryThread";
+import { getTopCategories } from "../../../services/DataService";
+import "./RightMenu.css";
+import { TopCategory } from "./TopCategory";
 
 export const RightMenu = () => {
   const { width } = useWindowDimensions();
+  const [topCategories, setTopCategories] = useState<
+    JSX.Element[] | undefined
+  >();
+
+  useEffect(() => {
+    getTopCategories().then((categoryThreads: CategoryThread[]) => {
+      const categoryThreadsByCategory: Dictionary<CategoryThread[]> = groupBy(
+        categoryThreads,
+        "category"
+      );
+      const topElements: JSX.Element[] = [];
+      for (let category in categoryThreadsByCategory) {
+        const categoryThreads: CategoryThread[] =
+          categoryThreadsByCategory[category];
+        topElements.push(
+          <TopCategory key={category} topCategories={categoryThreads} />
+        );
+      }
+      setTopCategories(topElements);
+    });
+  }, []);
 
   if (width <= MIN_DESKTOP_WINDOW_WIDTH) {
     return null;
   }
 
-  return <div className="right-menu">Right Menu</div>;
+  return <div className="right-menu right-menu-container">{topCategories}</div>;
 };
