96958a64e48d283004f72e1545112fab2fe40727
ekuzmichev <evgenii.e.kuzmichev@gmail.com>
2024-05-23 11:27:15 +0300

    Add more craco configurations

--- chapter12/super-forum-client/craco.config.js
+++ chapter12/super-forum-client/craco.config.js
@@ -1,9 +1,34 @@
+const path = require("path");
 const { VanillaExtractPlugin } = require("@vanilla-extract/webpack-plugin");
+const ModuleScopePlugin = require("react-dev-utils/ModuleScopePlugin");
 
 module.exports = {
   webpack: {
     plugins: {
       add: [new VanillaExtractPlugin()],
+    },
+  },
+  jest: {
+    configure: (jestConfig) => {
+      jestConfig.transform = {
+        "\\.css\\.ts$": "@vanilla-extract/jest-transform",
+        ...jestConfig.transform,
+      };
+      return jestConfig;
+    },
+  },
+  webpack: {
+    configure: (webpackConfig) => {
+      // ...
+
+      const moduleScopePlugin = webpackConfig.resolve.plugins.find(
+        (plugin) => plugin instanceof ModuleScopePlugin
+      );
+      moduleScopePlugin.allowedPaths.push(
+        path.resolve(__dirname, "node_modules/@vanilla-extract/webpack-plugin")
+      );
+
+      return webpackConfig;
     },
   },
 };
