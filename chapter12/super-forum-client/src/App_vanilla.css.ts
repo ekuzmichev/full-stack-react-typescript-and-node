d696faad33288e2c9d86030985805efd37568ecf
ekuzmichev <evgenii.e.kuzmichev@gmail.com>
2024-05-24 10:55:53 +0300

    Fix vars creation

--- chapter12/super-forum-client/src/App_vanilla.css.ts
+++ chapter12/super-forum-client/src/App_vanilla.css.ts
@@ -5,8 +5,8 @@ var border = createVar();
 
 export const screenRootContainer: string = style({
   vars: {
-    borderColor: "gray",
-    border: `solid 0.1em ${borderColor}`,
+    [borderColor]: "gray",
+    [border]: `solid 0.1em ${borderColor}`,
   },
   margin: "0 auto",
   maxWidth: 1200,
